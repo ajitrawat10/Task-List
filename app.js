1341348a4640dd5a00ddf6e78cc0a94e681b4fcf
AJIT-SINGH-RAWAT <47390463+AJIT-SINGH-RAWAT@users.noreply.github.com>
2020-01-29 19:43:21 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,33 @@
 
+const form=document.querySelector('#task-form');
+const tasklist=document.querySelector('.collection');
+const clearbtn=document.querySelector('.clear-tasks');
+const filter=document.querySelector('#filter');
+const taskinput=document.querySelector('#task');
+
+loadeventlistener();
+
+function loadeventlistener(){
+    form.addEventListener('submit',addtask);
+}
+
+function addtask(e){
+    if (taskinput.value===''){
+        alert('Add some task');
+    }
+
+    const li=document.createElement('li');
+    li.className='collection-item';
+    li.appendChild(document.createTextNode(taskinput.value));
+
+    
+    const link=document.createElement('a');
+    link.className='delete-item secondary-content';
+    link.innerHTML='<i class=" fa fa-remove" </i>';
+    li.appendChild(link);
+
+    tasklist.appendChild(li);
+
+    taskinput.value=''
+    e.preventDefault();
+}
