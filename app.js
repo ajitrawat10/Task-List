3afecdf4c066c01d63723a5177883d5f7878a7f3
AJIT-SINGH-RAWAT <47390463+AJIT-SINGH-RAWAT@users.noreply.github.com>
2020-01-30 23:09:07 +0530

    Update app.js

--- app.js
+++ app.js
@@ -40,3 +40,15 @@ function removetask(e){
 function cleartask(){
     tasklist.innerHTML='';
 }
+function filtertask(e){
+    const text=e.target.value.toLowerCase();
+    document.querySelectorAll('.collection-item').forEach(function(task){
+        const item = task.firstChild.textContent;
+        if (item.toLowerCase().indexOf(text)!=-1){
+            task.style.display = 'block';
+        }
+        else{
+            task.style.display = 'none';
+        }
+    })
+}
